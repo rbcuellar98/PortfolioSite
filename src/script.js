3d5d748f79260c0875c4847cfd1b7b1c53ee8863
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-02-10 18:48:00 -0500

    update

--- src/script.js
+++ src/script.js
@@ -13,6 +13,9 @@ const parameters = {
 
 gui
     .addColor(parameters, 'materialColor')
+    .onChange(()=>{
+        material.color.set(parameters.materialColor)
+    })
 
 /**
  * Base
@@ -24,14 +27,39 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Test cube
+ * Objects
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
-)
-scene.add(cube)
+// Texture
+const textureLoader = new THREE.TextureLoader()
+const gradientTexture = textureLoader.load('textures/gradients/3.jpg')
+// filter the image to its doesn't look ruff.
+// fit light intensity to the near image color
+gradientTexture.magFilter = THREE.NearestFilter
 
+// Material 
+const material = new THREE.MeshToonMaterial({color: parameters.materialColor, gradientMap: gradientTexture})
+// Meshes
+const mesh1 = new THREE.Mesh(
+    new THREE.TorusGeometry(1, 0.4,16,60),
+    material
+)
+const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(1,2,32),
+    material
+)
+const mesh3 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.8, 0.35,.100, 16),
+    material
+   // new THREE.MeshBasicMaterial({color: '#ff0000'})
+)
+// added objects to the scenes
+scene.add(mesh1, mesh2, mesh3);
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
+directionalLight.position.set(1,1,0);
+scene.add(directionalLight);
 /**
  * Sizes
  */
@@ -67,7 +95,8 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
